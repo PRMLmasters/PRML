81bf6b73b2019c89a4c3eb522e2f5a841147d389
垣谷悠斗 <hr.8714hmg@icloud.com>
2018-04-05 16:48:30 +0900

    comb()の訂正

--- impl/utils/function.py
+++ impl/utils/function.py
@@ -7,13 +7,36 @@ def integral(_func, start, end, n_split=500):
 
 
 def comb(n, r):
-    assert isinstance(n, 1) and isinstance(r, 1),
-    "n and r should be natural numbers"
+    """The number of combinations of N things taken k at a time.
+       This is known as "nCr".
+
+    Parameters
+    __________
+    n: int
+       Number of things.
+    r: int
+       Number of elements taken.
+
+    Returns
+    _______
+    comb_n[r]: int
+        The total number of combinations.
+
+    Notes
+    _____
+    - "comb_n[i]" represents "nCi"
+    - This algorithm is based on the formula,
+        nCr = nCr-1 * (n - r + 1) / r
+
+    """
+
+    assert isinstance(n, int), "n and r should be natural numbers"
+    assert isinstance(r, int), "n and r should be natural numbers"
     if n < r or n < 0:
         return 0
 
     comb_n = [0 for i in range(r+1)]
-    comb_n[0] = １
+    comb_n[0] = 1
     for i in range(r):
         comb_n[i+1] = comb_n[i] * (n - i) / (i + 1)
 
