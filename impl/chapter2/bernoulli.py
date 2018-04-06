a6b83c4d042d4bf3a6ec1fc8e94ec4c8b72d485b
田代真生 <masakitashiro@tashiromasaonoMacBook-puro.local>
2018-04-06 16:19:56 +0900

    BernoulliDistributionの引数をprobに変更しました

--- impl/chapter2/bernoulli.py
+++ impl/chapter2/bernoulli.py
@@ -5,7 +5,7 @@ from .distribution import DiscreteDistribution
 
 class BernoulliDistribution(DiscreteDistribution):
 
-    def __init__(self, probs):
+    def __init__(self, prob):
         super(BernoulliDistribution, self).__init__(prob,1-prob)
         self.prob = {
             0: 1 - prob,
