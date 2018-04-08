d75629d0be7a6ce661bc2a22133b522f9679762a
垣谷悠斗 <hr.8714hmg@icloud.com>
2018-04-08 09:54:56 +0900

    binomial distributionの実装

---
+++ impl/chapter2/binomial.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from ..utils.function.py import comb
+from .bernoulli import BernoulliDistribution
+
+
+class BinomialDistribution(BernoulliDistribution):
+
+    def __init__(self, mu, n):
+        super(BernoulliDistribution, self).__init__(mu)
+        self.n = n
+
+    def trial(self):
+        return np.random.choice(list(self.prob.keys()), size=self.n,
+                                p=list(self.prob.values()))
+
+    @property
+    def expected_value(self):
+        return self.n * prob[1]
+
+    @property
+    def variance(self):
+        return self.n * self.prob[1] * self.prob[0]
+
+    @property
+    def probability(self, m):
+        """The probability of m times of 1 in n times.
+           This is known as Bin(m|n, mu).
+
+        """
+
+        comb_n_r = comb(self.n, m)
+        return comb * (self.mu ** m) * ((1 - self.mu) ** (self.n - m))
