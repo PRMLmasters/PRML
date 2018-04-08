88f2f7be5a774df8a98106c620c3a8fb397ea0dd
垣谷悠斗 <hr.8714hmg@icloud.com>
2018-04-09 00:15:54 +0900

    引数のnの名前をn_trialsに変更

--- impl/chapter2/binomial.py
+++ impl/chapter2/binomial.py
@@ -6,21 +6,21 @@ from .bernoulli import BernoulliDistribution
 
 class BinomialDistribution(BernoulliDistribution):
 
-    def __init__(self, mu, n):
+    def __init__(self, mu, n_trials):
         super(BernoulliDistribution, self).__init__(mu)
-        self.n = n
+        self.n_trials = n_trials
 
     def trial(self):
-        return np.random.choice(list(self.prob.keys()), size=self.n,
+        return np.random.choice(list(self.prob.keys()), size=self.n_trials,
                                 p=list(self.prob.values()))
 
     @property
     def expected_value(self):
-        return self.n * prob[1]
+        return self.n_trials * prob[1]
 
     @property
     def variance(self):
-        return self.n * self.prob[1] * self.prob[0]
+        return self.n_trials * self.prob[1] * self.prob[0]
 
     def probability(self, m):
         """The probability of getting m times of 1 in n times.
@@ -28,4 +28,4 @@ class BinomialDistribution(BernoulliDistribution):
 
         """
 
-        return comb(self.n, m) * (self.mu ** m) * ((1 - self.mu) ** (self.n - m))
+        return comb(self.n_trials, m) * (self.mu ** m) * ((1 - self.mu) ** (self.n_trials - m))
